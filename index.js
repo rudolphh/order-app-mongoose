385b4f69d64b866851423e3fe7768afc6512dc57
Rudy A. Hernandez <rudolpharthur@gmail.com>
2021-03-19 01:14:17 -0700

    finished assignment

--- index.js
+++ index.js
@@ -12,13 +12,14 @@ let app = express();
 app.use(express.urlencoded({
     extended: true
 }));
+app.set('view engine', 'ejs');
 
 let orderModel = require('./models/order');
 
 
 // api endpoints
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname + '/public/index.html'));
+    res.render('index.ejs', { message : false });
 });
 
 app.post('/', (req, res) => {
@@ -37,21 +38,75 @@ app.post('/', (req, res) => {
         if(err) res.status(500).send(err)
         else {
             console.log(data);
-            res.send('data inserted');
+            res.render('index.ejs', { message : "order completed"});
         }
     });
 
 });
 
-app.get('/orders', (req, res) => {
+
+app.get('/admin', (req, res) => {
+
     orderModel.find((err, data) => {
         if(err) res.status(500).send(err);
         else {
-            res.json(data);
+
+            data.forEach((order) => {
+                let currentDate = new Date();
+                let orderDate = new Date(order.createdAt.valueOf());
+                orderDate.setDate(orderDate.getDate() + 1);
+
+                if(currentDate < orderDate) {
+                    order.status = "in progress";
+                    return;
+                } 
+
+                orderDate.setDate(orderDate.getDate() + 1);
+
+                if(currentDate < orderDate) {
+                    order.status = "dispatched";
+                    return;
+                } else {
+                    order.status = "delivered";
+                }
+
+            });
+
+            res.render('admin.ejs', { 
+                orders: data
+            });
         }
     });
 });
 
+
+app.post('/sendEmail', (req, res) =>{
+    let order = req.body.order;
+    let email = req.body.email;
+    let user = req.body.user;
+    let status = req.body.status;
+
+    const sgMail = require('@sendgrid/mail');
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+    const msg = {
+    to: email, // Change to your recipient
+    from: 'ru@rudyah.com', // Change to your verified sender
+    subject: 'Status of Order: ' + order,
+    text: 'Hey ' + user + ', your order is ' + status,
+    html: '<strong>Hey ' + user + ', your order is ' + status + '</strong>',
+    };
+
+    sgMail.send(msg)
+    .then(() => {
+        console.log('Email sent');
+        res.send('Email sent');
+    })
+    .catch((error) => {
+        console.error(error)
+    });
+
+});
 
 
 let port = 3000;
